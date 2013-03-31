d696c36b4cfddf609cd210ee987bf740030af4bf
C. Benson Manica <cbmanica@gmail.com>
2013-03-31 16:03:12 -0700

    Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Camille::Application.config.secret_token = '2a7f8d5f2935261ff20d246c369c2350641169d89cbc2046c646545506ed0601476dff105a59a2dbb201ed8804db4be6eef1b30c27df0d21e3d9f52c9c16cd8b'
